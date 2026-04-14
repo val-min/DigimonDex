b85422025b0ba281c5e88a286960ce291e8b7d9b
val-min <valenciavalencia709@gmail.com>
2026-04-14 18:28:11 +0700

    Initial Commit

---
+++ DigimonDex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DigimonDex
+//
+//  Created by Valencia Sutanto on 14/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DigimonDex/DigimonDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigimonDexApp.swift
+//  DigimonDex
+//
+//  Created by Valencia Sutanto on 14/04/26.
+//
+
+import SwiftUI
+
+@main
+struct DigimonDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
